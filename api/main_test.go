c4d733db795169eaa0c038c413da329cbc48ef5a
Alan <kcykey@gmail.com>
2023-11-14 19:51:32 +0800

    Add token into server

--- api/main_test.go
+++ api/main_test.go
@@ -3,9 +3,25 @@ package api
 import (
 	"os"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
+	db "github.com/YuanData/allegro-trade/db/sqlc"
+	"github.com/YuanData/allegro-trade/util"
 )
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSecretKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
+
+	return server
+}
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
--- api/server.go
+++ api/server.go
@@ -1,26 +1,48 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	db "github.com/YuanData/allegro-trade/db/sqlc"
+	"github.com/YuanData/allegro-trade/token"
+	"github.com/YuanData/allegro-trade/util"
 )
 
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	config     util.Config
+	store      db.Store
+	tokenAuthzr token.Authzr
+	router     *gin.Engine
 }
 
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
-	router := gin.Default()
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenAuthzr, err := token.NewJWTAuthzr(config.TokenSecretKey)
+	if err != nil {
+		return nil, fmt.Errorf("token authzr err: %w", err)
+	}
+
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenAuthzr: tokenAuthzr,
+	}
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("symbol", validSymbol)
 	}
 
+	server.setupRouter()
+	return server, nil
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
+
 	router.POST("/members", server.createMember)
+	router.POST("/members/login", server.loginMember)
 
 	router.POST("/traders", server.createTrader)
 	router.GET("/traders/:id", server.getTrader)
@@ -29,7 +51,6 @@ func NewServer(store db.Store) *Server {
 	router.POST("/records", server.createRecord)
 
 	server.router = router
-	return server
 }
 
 func (server *Server) Start(address string) error {
